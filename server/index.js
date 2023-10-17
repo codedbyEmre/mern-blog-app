4dd4c571fc0a310b3cd0dd87ab0f9c406153cc4c
codedbyemre <codedbyemre@gmail.com>
2023-10-17 12:10:51 +0300

    cors added to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const port = process.env.PORT || 8000;
 const blogRoutes = require('./routes/blogRoutes');
 
@@ -10,6 +11,7 @@ const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
