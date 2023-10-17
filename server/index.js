db3833d698cca19f36272e6c6bba2cb4c0955a65
codedbyemre <codedbyemre@gmail.com>
2023-10-17 11:09:51 +0300

    mongodb connection set and vercel.json added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // imports
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 8000;
 const blogRoutes = require('./routes/blogRoutes');
 
@@ -10,7 +11,15 @@ const app = express();
 // routes
 app.use('/api/blogs', blogRoutes);
 
-// listen for request
-app.listen(port, () => {
-  console.log('app listening on port', port);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(port, () => {
+      console.log('connected to db & app listening on port', port);
+    });
+  })
+  .catch(err => {
+    console.log('err :>> ', err);
+  });
